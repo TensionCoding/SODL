4831548c572a4544f63ab56d1cc120314ef57d6f
TensionCoding <wdd4services@outlook.com>
2022-09-03 11:40:29 -0400

    added redux, now adding redux toolkit

--- src/redux/actions/countAction.js
+++ src/redux/actions/countAction.js
@@ -1,11 +1,13 @@
+import {COUNT_INCREASE, COUNT_DECREASE} from '../types';
+
 export const increment = () => {
   return {
-    type: 'COUNT_INCREASE',
+    type: COUNT_INCREASE,
   };
 };
 
 export const decrement = () => {
   return {
-    type: 'COUNT_DECREASE',
+    type: COUNT_DECREASE,
   };
 };
--- src/redux/reducers/countReducer.js
+++ src/redux/reducers/countReducer.js
@@ -1,15 +1,18 @@
-const initialState = {
+import {combineReducers} from 'redux';
+import {COUNT_INCREASE, COUNT_DECREASE} from '../types';
+
+const INITIAL_STATE = {
   count: 0,
 };
 
-export default (state = initialState, action) => {
+export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case 'COUNT_INCREASE':
+    case COUNT_INCREASE:
       return {
         ...state,
         count: state.count + 1,
       };
-    case 'COUNT_DECREASE':
+    case COUNT_DECREASE:
       return {
         ...state,
         count: state.count - 1,
